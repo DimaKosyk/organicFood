ad278aba0210a0f4f5f8be48d542f10895db720e
DimaKosyk <dimakosyk93@gmail.com>
2022-02-07 00:28:27 +0200

    added comments and application section

---
+++ #src/js/rateyo.js
@@ -0,0 +1,10 @@
+$(function () {
+  $('.rating').rateYo({
+    starWidth: "23.15px",
+    spacing: "9px",
+    normalFill: "#CCCCCC",
+    ratedFill: "#F1B90B",
+    readOnly: true,
+    starSvg: '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip - path="url(#clip0_32_284)"><path d="M11.7708 17.6153L4.96684 21.4238L6.48622 13.7758L0.760803 8.48161L8.50435 7.56322L11.7708 0.482422L15.0372 7.56322L22.7808 8.48161L17.0553 13.7758L18.5747 21.4238L11.7708 17.6153Z"/></g><defs><clipPath id="clip0_32_284"><rect width="23.1525" height="23.1525" fill="white" transform="translate(0.194336)" /></clipPath></defs></svg>'
+  });
+});
