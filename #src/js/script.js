f23b854fdf3b9327ea54c4fef7caa6b6cdd34007
DimaKosyk <dimakosyk93@gmail.com>
2022-02-08 00:17:20 +0200

    header adaptive

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,3 +1,9 @@
 @@include('./testwebp.js')
 @@include('./slick.js')
 @@include('./rateyo.js')
+
+$(function () {
+  $('.header__menu-btn').on('click', function (){
+    $('.header__menu-list').toggleClass('header__menu-list--active');
+  });
+});
